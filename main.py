0a187d186fcb5dac653a04e5817409503cd021d1
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2025-01-24 00:49:54 +0300

    funny things

--- main.py
+++ main.py
@@ -4,95 +4,102 @@ import re
 import vk_api
 from vk_api.utils import get_random_id
 import time
-import os 
+import os
 from youtubers import youtubers_list
 
-VK_TOKEN = os.getenv(TOKEN_VK) #также токен можно указать напрямую в файле VK_TOKEN = "ваш_токен"
-vk_session = vk_api.VkApi(token=VK_TOKEN)
-upload = vk_api.VkUpload(vk_session)
-vk = vk_session.get_api()
 
-# user_id = заменить на ваш айди в вк  
-user_id = os.getenv(USER_ID) #аналогично можно указать напрямую в файле как и токен
+def main():
+    VK_TOKEN = os.getenv('TOKEN_VK')
+    vk_session = vk_api.VkApi(token=VK_TOKEN)
+    upload = vk_api.VkUpload(vk_session)
+    vk = vk_session.get_api()
+    ydl_opts = {"ffmpeg_location": "D:/Soft/FFmpeg/bin"} # Manual location
 
-while True:
-    try:
-        for youtuber in youtubers_list:
-            print("получаем хтмл")
-            html = requests.get(f"https://www.youtube.com/@{youtuber}/videos").text
-            print("проверяем последнее видео")
-            URL = (
-                "https://www.youtube.com/watch?v="
-                + re.search('(?<="videoId":").*?(?=")', html).group()
+    user_id = os.getenv('USER_ID')
+
+    while True:
+        try:
+            for youtuber in youtubers_list:
+                print("CONSOLE | Получение HTML")
+                html = requests.get(f"https://www.youtube.com/@{youtuber}/videos").text
+                print("CONSOLE | Проверка последнего видео")
+
+                URL = f"https://www.youtube.com/watch?v={re.search(r'videoId":"(.*?)"', html).group(1)}"
+
+                with open("downloaded.txt", "r+") as file:
+                    content = file.read()
+
+                    if URL not in content:
+                        ydl_opts = {
+                            "format": "bestvideo+bestaudio/best",
+                            "concurrent-fragments": "4",
+                            "outtmpl": "video.%(ext)s",
+                            "ignoreerrors": True,
+                            "no-overwrites": True,
+                            "continue": True,
+                            "writethumbnail": True,
+                            "embedthumbnail": True,
+                            "convert-thumbnails": "jpg",
+                            "postprocessors": [
+                                {
+                                    "format": "jpg",
+                                    "key": "FFmpegThumbnailsConvertor",
+                                    "when": "before_dl",
+                                }
+                            ],
+                        }
+
+                        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                            try:
+                                info_dict = ydl.extract_info(URL, download=True)
+                                video_channel = info_dict.get("channel", None)
+                                video_title = info_dict.get("title", None)
+                                video_format = info_dict.get("ext", None)
+                                video_name = f"[{video_channel}] {video_title}"
+                                file_name = f"video.{video_format}"
+                            except Exception as e:
+                                vk.messages.send(
+                                    user_id=user_id,
+                                    random_id=get_random_id(),
+                                    message=f"CONSOLE | ОШИБКА! СКАЧИВАНИЕ ВИДЕО {video_name} НЕ УДАЛОСЬ: {str(e)}",
+                                )
+                                break
+
+                        print("CONSOLE | Загрузка видео в VK")
+                        response = upload.video(
+                            video_file=file_name,
+                            name=video_name,
+                            group_id=youtuber.group_id,
+                            album_id=youtuber.pl_id,
+                            wallpost="1",
+                        )
+                        print("CONSOLE | Загрузка обложки видео")
+                        video_id = response.get("video_id")
+                        response = upload.thumb_video(
+                            photo_path="/root/video.jpg",
+                            owner_id=f"-{youtuber.group_id}",
+                            video_id=video_id,
+                        )
+                        vk.messages.send(
+                            user_id=user_id,
+                            random_id=get_random_id(),
+                            message=f"CONSOLE | Видео {video_name} успешно загружено в группу",
+                        )
+                        file.write(URL + "\n")
+                        os.remove(file_name)
+                        os.remove("video.jpg")
+
+                print("CONSOLE | Перерыв 10 минут")
+                time.sleep(10 * 60)
+
+        except Exception as e:
+            vk.messages.send(
+                user_id=user_id,
+                random_id=get_random_id(),
+                message=f"CONSOLE | Программа завершена из-за ошибки. Перезапустите скрипт: {str(e)}",
             )
+            break
 
-            with open("downloaded.txt", "r+") as file:
-                content = file.read()
 
-                if URL not in content:
-                    ydl_opts = {
-                        "format": "bestvideo+bestaudio/best",
-                        "concurrent-fragments": "4",
-                        "outtmpl": "video.%(ext)s",
-                        "ignoreerrors": True,
-                        "no-overwrites": True,
-                        "continue": True,
-                        "writethumbnail": True,
-                        "embedthumbnail": True,
-                        "convert-thumbnails": "jpg",
-                        "postprocessors": [
-                            {
-                                "format": "jpg",
-                                "key": "FFmpegThumbnailsConvertor",
-                                "when": "before_dl",
-                            }
-                        ],
-                    }
-
-                    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                        try:
-                            info_dict = ydl.extract_info(URL, download=True)
-                            video_channel = info_dict.get("channel", None)
-                            video_title = info_dict.get("title", None)
-                            video_format = info_dict.get("ext", None)
-                            video_name = f"[{video_channel}] {video_title}"
-                            file_name = f"video.{video_format}"
-                        except:
-                            vk.messages.send(
-                                user_id=user_id,
-                                random_id=get_random_id(),
-                                message=f"ОШИБКА! ВИДЕО {video_name} СКАЧАТЬ НЕ УДАЛОСЬ",
-                            )
-                            break
-                    print("загрузка видео в вк")
-                    response = upload.video(
-                        video_file=file_name,
-                        name=video_name,
-                        group_id=youtuber.group_id,
-                        album_id=youtuber.pl_id,
-                        wallpost="1",
-                    )
-                    print("загрузка обложки")
-                    video_id = response.get("video_id")
-                    response = upload.thumb_video(
-                        photo_path="/root/video.jpg", #здесь нужно указать прямой путь до папки с программой в формате "/путь/к/файлу/video.jpg"
-                        owner_id=f"-{group_id}",
-                        video_id=video_id,
-                    )
-                    vk.messages.send(
-                        user_id=user_id,
-                        random_id=get_random_id(),
-                        message=f"Видео {video_name} успешно загружено в группу",
-                    )
-                    file.write(URL + "\n")
-                    os.remove(file_name)
-                    os.remove("video.jpg")
-        print("перерыв 10 минут")
-        time.sleep(10 * 60)
-    except:
-        vk.messages.send(
-            user_id=user_id,
-            random_id=get_random_id(),
-            message="Программа завершена из-за ошибки. Перезапустите скрипт.",
-        )
-        break
+if __name__ == "__main__":
+    main()
--- youtubers.py
+++ youtubers.py
@@ -1,16 +1,14 @@
 class Youtuber:
     def __init__(self, name, group_id, pl_id=None):
-        self.name = name #имя ютубера после @
-        self.group_id = group_id 
-        self.pl_id = pl_id #номер плейлиста в группе вк
-        
+        self.name = name  # имя ютубера после @
+        self.group_id = group_id  # ID группы ВК
+        self.pl_id = pl_id  # номер плейлиста в группе ВК
+
     def __str__(self):
         return self.name
-        
+
+# Пример правильного определения youtubers_list, без ограничений количества
 youtubers_list = [
-    Youtuber(name, group_id, pl_id),   #короткая ссылка на ютубера в поле name, айди группы в вк в поле group_id, айди плейлиста в поле pl_id
-    Youtuber(name, group_id, pl_id),   
-    Youtuber(name, group_id, pl_id),   
-    Youtuber(name, group_id, pl_id),   
-    # и так далее
+    Youtuber(name="NULL", group_id="NULL", pl_id=NULL),
+    Youtuber(name="NULL", group_id="NULL", pl_id=NULL)
 ]
